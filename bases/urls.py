22835b1b2ccb62a095dcb01876038212ab3bfa36
Henry <hvilmur686@g.educaand.es>
2023-10-28 01:54:56 +0200

    menu sidebar view

--- bases/urls.py
+++ bases/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('',Home.as_view(),name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='bases/login.html'),name='logout'),
+
+
+
+    path('catalogo/categorias',Home.as_view(),name='categorias'),
+    path('catalogo/subcategorias',Home.as_view(),name='subcategorias'),
+    path('movimientos/compras',Home.as_view(),name='compras'),
 ]
