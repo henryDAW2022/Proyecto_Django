336e6fdbb380a53c0caa4350a65c0f90131699cd
Henry <hvilmur686@g.educaand.es>
2023-10-27 20:50:30 +0200

    vista login editada

--- bases/urls.py
+++ bases/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 from bases.views import *
 
@@ -6,4 +8,6 @@ app_name = "config"
 
 urlpatterns = [
     path('',Home.as_view(),name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='bases/login.html'),name='logout'),
 ]
