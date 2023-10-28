0cf1f57b600e54aafd43b36442baa560af363af3
Henry <hvilmur686@g.educaand.es>
2023-10-28 07:13:51 +0200

    vista usuarios, config datatables

--- bases/urls.py
+++ bases/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='bases/login.html'),name='logout'),
 
-
+    ## rutas lista usuarios
+    path('users-lists',UserList.as_view(),name='users_list'),
 
     path('catalogo/categorias',Home.as_view(),name='categorias'),
     path('catalogo/subcategorias',Home.as_view(),name='subcategorias'),
--- bases/views.py
+++ bases/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import *
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.models import Group, Permission
+
+
+from .models import Usuario
 
 
 class Home(LoginRequiredMixin,TemplateView):
     template_name = 'bases/home.html'
     login_url = 'config:login'
+
+class UserList(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    template_name ='bases/users_list.html'
+    login_url = 'config:login'
+    model = Usuario
+    permission_required = 'bases:view_usuario'
+    context_object_name = 'obj'
+    
